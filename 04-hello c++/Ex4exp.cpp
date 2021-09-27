e31930d4d5a773a600e816c61c7fa7529765c78c
Sofichh <90448499+Sofichh@users.noreply.github.com>
2021-09-27 21:04:40 +0300

    Add files via upload

---
+++ 04-hello c++/Ex4exp.cpp
@@ -0,0 +1,12 @@
+// ConsoleApplication2.cpp : Defines the entry point for the console application.
+//
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+int main(){
+cout << "Hello, World!" << endl;
+cin.get();
+cin.get();
+return 0;
+}
+
