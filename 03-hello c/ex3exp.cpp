a3a117029e41dedfa06542872ba4aa6d09c9bcd6
Sofichh <90448499+Sofichh@users.noreply.github.com>
2021-09-27 21:11:29 +0300

    Delete ex3exp.cpp

--- 03-hello c/ex3exp.cpp
+++
@@ -1,9 +0,0 @@
-// ConsoleApplication3.cpp : Defines the entry point for the console application.
-//
-
-#include "stdafx.h"
-#include <stdio.h>
-int main(){
-    printf("Hello, World!\n");
-    return 0;
-}
